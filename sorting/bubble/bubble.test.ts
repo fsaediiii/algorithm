c7d7fe8c3f0aec54d6c2678a078e1852646bdc54
saedi.f <fatemehsd74@gmail.com>
2025-12-20 11:26:58 +0330

    feat: add recursive bubble sort

--- sorting/bubble/bubble.test.ts
+++ sorting/bubble/bubble.test.ts
@@ -7,23 +7,54 @@ describe("BubbleSort", () => {
     sorter = new BubbleSort();
   });
 
-  test("sorts an array of numbers in ascending order", () => {
-    const input = [4, 5, 2, 1, 6, 3, 7];
-    const expected = [1, 2, 3, 4, 5, 6, 7];
-    expect(sorter.sort(input)).toEqual(expected);
+  describe("iterative sort()", () => {
+    test("sorts an array of numbers in ascending order", () => {
+      const input = [4, 5, 2, 1, 6, 3, 7];
+      const expected = [1, 2, 3, 4, 5, 6, 7];
+      expect(sorter.sort(input)).toEqual(expected);
+    });
+
+    test("works with repeated numbers", () => {
+      const input = [3, 1, 2, 3, 1];
+      const expected = [1, 1, 2, 3, 3];
+      expect(sorter.sort(input)).toEqual(expected);
+    });
+
+    test("returns an empty array when input is empty", () => {
+      expect(sorter.sort([])).toEqual([]);
+    });
+
+    test("returns a single element array as is", () => {
+      expect(sorter.sort([42])).toEqual([42]);
+    });
+
+    test("does not mutate the original array", () => {
+      const input = [3, 2, 1];
+      const copy = [...input];
+      sorter.sort(input);
+      expect(input).toEqual(copy);
+    });
   });
 
-  test("works with repeated numbers", () => {
-    const input = [3, 1, 2, 3, 1];
-    const expected = [1, 1, 2, 3, 3];
-    expect(sorter.sort(input)).toEqual(expected);
-  });
+  describe("recursive recursionSort()", () => {
+    test("sorts an array of numbers in ascending order", () => {
+      const input = [4, 5, 2, 1, 6, 3, 7];
+      const expected = [1, 2, 3, 4, 5, 6, 7];
+      expect(sorter.recursionSort([...input])).toEqual(expected);
+    });
 
-  test("returns an empty array when input is empty", () => {
-    expect(sorter.sort([])).toEqual([]);
-  });
+    test("works with repeated numbers", () => {
+      const input = [3, 1, 2, 3, 1];
+      const expected = [1, 1, 2, 3, 3];
+      expect(sorter.recursionSort([...input])).toEqual(expected);
+    });
 
-  test("returns a single element array as is", () => {
-    expect(sorter.sort([42])).toEqual([42]);
+    test("returns an empty array when input is empty", () => {
+      expect(sorter.recursionSort([])).toEqual([]);
+    });
+
+    test("returns a single element array as is", () => {
+      expect(sorter.recursionSort([42])).toEqual([42]);
+    });
   });
 });
--- sorting/bubble/bubble.ts
+++ sorting/bubble/bubble.ts
@@ -20,4 +20,18 @@ export default class BubbleSort {
 
     return result;
   }
+
+  recursionSort(ary: number[], pointer: number = 0, aryCount: number = ary.length): number[] {
+    if (aryCount <= 1) return ary;
+
+    if (pointer === aryCount - 1) {
+      return this.recursionSort(ary, 0, aryCount - 1);
+    }
+
+    if (ary[pointer] > ary[pointer + 1]) {
+      [ary[pointer], ary[pointer + 1]] = [ary[pointer + 1], ary[pointer]];
+    }
+
+    return this.recursionSort(ary, pointer + 1, aryCount);
+  }
 }
